5f5427ab9d6f2094f0b35ec82fe177956a36c22b
Catherine <catherinema828@gmail.com>
2018-05-30 23:56:55 -0700

    Fixed bugs in preprocess code, improved speed

--- training/preprocess.py
+++ training/preprocess.py
@@ -1,72 +1,87 @@
 import numpy as np
+from key import OUTPUT_SIZE, DASH, DASH_POW, DASH_DEG, TURN, TURN_DEG, TACKLE, TACKLE_DEG, KICK, KICK_POW, KICK_DEG
 
 LOG_FILE = 'base_left-11.log'
-OUTPUT_SIZE = 10
+INPUT_SIZE = 68
 ACTIONS = {'Dash', 'Turn', 'Tackle', 'Kick'}
 
 def fetch_data(log_file):
-	X = []
-	y = []
-	i = 0
+    X = []
+    y = []
+    prev_iter = 0
+    features_exist = False
+    action_exists = False
+    prev_valid = False
 
-	with open(log_file) as f:
-		lines = f.readlines()
-	lines = [line.strip() for line in lines]
+    with open(log_file) as f:
+        for line in f:
+            line = line.strip()
+            words = line.split()
+            curr_iter = int(words[0])
 
-	while i < len(lines):
-		words = lines[i].split()
+            if curr_iter != prev_iter:
+                if prev_valid:
+                    X.append(state_features)
+                    y.append(action)
+                    prev_valid = False
 
-		if words[3] == 'StateFeatures':
-			action_exists = False
-			for action in ACTIONS:
-				if action in lines[i+1]:
-					action_exists = True
+            if words[3] == 'StateFeatures':
+                state_features = get_feature_array(words[4:])
+                features_exist = True
+            else:
+                action_exists = False
+                for action in ACTIONS:
+                    if action in line:
+                        action_exists = True
 
-			if action_exists:
-				state_features = get_feature_array(words[4:])
-				X.append(state_features)
+                if action_exists and features_exist:
+                    action = get_action_array(words[4])
+                    prev_valid = True
+                else:
+                    features_exist = False
 
-				action = get_action_array(lines[i+1].split()[4])
-				y.append(action)
+            prev_iter = curr_iter
 
-				i += 2
-			else:
-				i += 1
-		else:
-			i += 1
-	
-	return np.array(X), np.array(y)
+        if prev_valid:
+            X.append(state_features)
+            y.append(action)
+
+    return np.array(X), np.array(y)
 
 def get_feature_array(features):
-	feature_array = [float(feat) for feat in features]
-	for feat in feature_array:
-		assert feat >= -1.0 and feat <= 1.0
-	return feature_array
+    feature_array = [float(feat) for feat in features]
+    assert len(feature_array) == INPUT_SIZE
+
+    for feat in feature_array:
+        assert feat >= -1. and feat <= 1.
+
+    return feature_array
 
 def get_action_array(action):
-	action_array = [0 for i in range(OUTPUT_SIZE)]
-	param_str = action[action.index('(')+1:-1]
+    action_array = [0. for i in range(OUTPUT_SIZE)]
+    param_str = action[action.index('(')+1:-1]
+    action_type = action.split('(')[0]
 
-	if action[:4] == 'Dash':
-		params = [float(param) for param in param_str.split(',')]
-		action_array[0] = 1
-		action_array[1] = params[0]
-		action_array[2] = params[1]
-	elif action[:4] == 'Turn':
-		param = float(param_str)
-		action_array[3] = 1
-		action_array[4] = param
-	elif action[:6] == 'Tackle':
-		params = [float(param) for param in param_str.split(',')]
-		action_array[5] = 1
-		action_array[6] = params[0]
-	elif action[:4] == 'Kick':
-		params = [float(param) for param in param_str.split(',')]
-		action_array[7] = 1
-		action_array[8] = params[0]
-		action_array[9] = params[1]
+    if action_type == 'Dash':
+        params = [float(param) for param in param_str.split(',')]
+        action_array[DASH] = 1.
+        action_array[DASH_POW] = params[0]
+        action_array[DASH_DEG] = params[1]
+    elif action_type == 'Turn':
+        param = float(param_str)
+        action_array[TURN] = 1.
+        action_array[TURN_DEG] = param
+    elif action_type == 'Tackle':
+        params = [float(param) for param in param_str.split(',')]
+        action_array[TACKLE] = 1.
+        action_array[TACKLE_DEG] = params[0]
+    elif action_type == 'Kick':
+        params = [float(param) for param in param_str.split(',')]
+        action_array[KICK] = 1.
+        action_array[KICK_POW] = params[0]
+        action_array[KICK_DEG] = params[1]
 
-	return action_array
+    return action_array
 
 if __name__ == '__main__':
-	X, y = fetch_data(LOG_FILE)
+    X, y = fetch_data(LOG_FILE)
