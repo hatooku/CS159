9e0480206569948d0299a2f1d6ae7afbf4cb25a3
Catherine <catherinema828@gmail.com>
2018-05-30 20:00:23 -0700

    Fixing bugs in preprocess script

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ import numpy as np
 
 LOG_FILE = 'base_left-11.log'
 OUTPUT_SIZE = 10
+ACTIONS = {'Dash', 'Turn', 'Tackle', 'Kick'}
 
 def fetch_data(log_file):
 	X = []
@@ -16,13 +17,21 @@ def fetch_data(log_file):
 		words = lines[i].split()
 
 		if words[3] == 'StateFeatures':
-			state_features = get_feature_array(words[4:])
-			X.append(state_features)
+			action_exists = False
+			for action in ACTIONS:
+				if action in lines[i+1]:
+					action_exists = True
 
-			action = get_action_array(lines[i+1].split()[4])
-			y.append(action)
+			if action_exists:
+				state_features = get_feature_array(words[4:])
+				X.append(state_features)
 
-			i += 2
+				action = get_action_array(lines[i+1].split()[4])
+				y.append(action)
+
+				i += 2
+			else:
+				i += 1
 		else:
 			i += 1
 	
@@ -48,9 +57,9 @@ def get_action_array(action):
 		action_array[3] = 1
 		action_array[4] = param
 	elif action[:6] == 'Tackle':
-		param = float(param_str)
+		params = [float(param) for param in param_str.split(',')]
 		action_array[5] = 1
-		action_array[6] = param
+		action_array[6] = params[0]
 	elif action[:4] == 'Kick':
 		params = [float(param) for param in param_str.split(',')]
 		action_array[7] = 1
@@ -59,5 +68,5 @@ def get_action_array(action):
 
 	return action_array
 
-if name == '__main__':
+if __name__ == '__main__':
 	X, y = fetch_data(LOG_FILE)
